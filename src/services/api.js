5850d92b63a3047bd63a86a07ebdd836d48fe4c8
Navya Sri <admin@MacBook-Air-2.lan>
2025-01-07 13:29:48 +0530

    new

--- src/services/api.js
+++ src/services/api.js
@@ -1,11 +1,25 @@
+// import axios from 'axios';
+
+// const backendUrl = 'http://localhost:5000'; 
+
+// export const runCode = async (code) => {
+//   try {
+//     const response = await axios.post(`${backendUrl}/run_code`, { code });
+//     return response.data;
+//   } catch (error) {
+//     console.error('Error running code:', error);
+//     throw error;
+//   }
+// };
 import axios from 'axios';
 
-const backendUrl = 'http://localhost:5000'; 
+const backendUrl = 'https://python-code-editor-dli9.onrender.com'; // Updated to the deployed backend URL
 
 export const runCode = async (code) => {
   try {
-    const response = await axios.post(`${backendUrl}/run_code`, { code });
-    return response.data;
+    // Send the code to the backend's /execute route
+    const response = await axios.post(`${backendUrl}/execute`, { code });
+    return response.data; // Returns the output from the backend
   } catch (error) {
     console.error('Error running code:', error);
     throw error;
